5c0b392cbb697197fd0253d9d7a808ebd9e11678
dongyunjin <292911664@qq.com>
2020-02-18 23:42:43 +0800

    去除冗余注解

--- src/main/java/com/cn/employment/EmploymentApplication.java
+++ src/main/java/com/cn/employment/EmploymentApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 @MapperScan(basePackages= {"com.cn.employment.mapper"})
 public class EmploymentApplication {
 
