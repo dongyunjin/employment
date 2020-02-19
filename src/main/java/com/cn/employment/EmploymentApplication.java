57628410d6e0c8bbc75d7531891b63d0a2493c88
dongyunjin <292911664@qq.com>
2020-02-19 09:24:03 +0800

    去除冗余注解

--- src/main/java/com/cn/employment/EmploymentApplication.java
+++ src/main/java/com/cn/employment/EmploymentApplication.java
@@ -3,7 +3,6 @@ package com.cn.employment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages= {"com.cn.employment.mapper"})
