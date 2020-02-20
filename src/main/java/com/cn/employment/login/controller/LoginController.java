5522d3e584be18114b206fe6543d557d270ecba5
dongyunjin <292911664@qq.com>
2020-02-20 09:34:29 +0800

    登录修改

--- src/main/java/com/cn/employment/login/controller/LoginController.java
+++ src/main/java/com/cn/employment/login/controller/LoginController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -29,7 +26,7 @@ public class LoginController {
     */
     @ResponseBody
     @PostMapping("login")
-    public Long login(Map<String, String> map) {
+    public Long login(@RequestParam Map<String, String> map) {
         return this.usersService.queryUserId(map);
     }
 
