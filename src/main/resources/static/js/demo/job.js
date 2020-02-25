4aff5fe05f323ff70dc1cdda804c54ff7aeddc07
Zhou Pengfei <zhoupengfei@horiscale.com>
2020-02-25 23:31:20 +0800

    Add dependency package about ztree and easyui-layout
    frontend/job front/main just a test page of normal users

--- src/main/resources/static/js/demo/job.js
+++ src/main/resources/static/js/demo/job.js
@@ -7,45 +7,55 @@ var Course = {
     // sTable: null
 };
 
+$(function(){
+    var setting={
+        check: {
+            enable: true,
+            chkStyle: "checkbox"//显示 checkbox 选择框，默认checkbox可选择值radio
+        }
+    };
+    var zTreeNodes=[
+        {"name":"北京",
+        	"open":true,
+        	children:[
+                {"name":"东城区"},
+                {"name":"朝阳区"}
+            ]
+        },//open:true表示默认展开
+        {"name":"重庆",
+        	"open":true,
+        	children:[
+                {
+                	"name":"巴南区",
+                    children:[
+                        {"name":"南泉"},
+                        {"name":"界石"}
+                    ]
+                },
+                {"name":"渝中区"}
+            ]
+        }
+    ];
+    var city = $.fn.zTree.init($("#tree"), setting, zTreeNodes);    
+    var setting={
+        check: {
+            enable: true,
+            chkStyle: "checkbox"//显示 checkbox 选择框，默认checkbox可选择值radio
+        },
+        callback:{
+            onCheck:zTreeOnCheck
+        }
+    };
+    var city = $.fn.zTree.init($("#tree"), setting, zTreeNodes);
+})
+function zTreeOnCheck(event, treeId, treeNode) {
+     alert(treeNode.name);//弹出城市名字
+}
 
-
-$(document).ready(function () {
-	var pTable = $('#pTable').DataTable({
-		select: {
-			style: "single"
-		},
-		searching: true,
-		bLengthChange: true,
-		aLengthMenu: [10],
-		pagingType: "simple",
-		oLanguage: {
-			sLengthMenu: "每页显示 _MENU_ 条记录",
-			sZeroRecords: "对不起，查询不到任何相关数据",
-			sInfo: "当前显示 _START_ 到 _END_ 条，共 _TOTAL_条记录",
-			sInfoEmtpy: "找不到相关数据",
-			sInfoFiltered: "数据表中共为 _MAX_ 条记录)",
-			sProcessing: "正在加载中...",
-			sSearch: "搜索",
-			oPaginate: {
-				sFirst: "第一页",
-				sPrevious: " 上一页 ",
-				sNext: " 下一页 ",
-				sLast: " 最后一页 "
-			},
-		},
-		processing: false,
-		ajax: "trade",
-		columns: [
-			{
-				data: "trade_item",
-			}
-		],
-		
-	});
-
+/*$(document).ready(function () {
 	var sTable = $('#sTable').DataTable({
 		select: {
-			style: "single"
+			style: "single",
 		},
 		searching: true,
 		bLengthChange: true,
@@ -81,21 +91,9 @@ $(document).ready(function () {
 		]
 	});
 
-	$('#pTable tbody').on('click', 'tr', function () {
-		var id = pTable.row(this).data().id;
-		$("#memberid").val(id);
-		var url = "selectByMemberId/" + id;
-		calendar.getEventSources().filter(function (event) {
-			event.remove();
-		})
-//	  debugger;
-//	  calendar.getEvents().filter(function(event){
-//		  event.remove();
-//	  })
-		calendar.addEventSource(url);
-	});
+	
 
-    $('#sTable tbody').on('click', 'tr', function () {
+   /* $('#sTable tbody').on('click', 'tr', function () {
         var id = sTable.row(this).data().id;
         $("#memberid").val(id);
         var url = "selectByMemberId/" + id;
@@ -108,6 +106,6 @@ $(document).ready(function () {
 //	  })
         calendar.addEventSource(url);
     });
-});
+});*/
 	
 	
